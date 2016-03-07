0d638d292950ff4fac610884f735b35ef802b1d0
Raymond Elferink <raymond@raycom.com>
2016-03-07 14:17:05 +0100

    improvements and decoupling of chat-add

--- dist/stack.chat.js
+++ dist/stack.chat.js
@@ -33,38 +33,29 @@ angular.module('stack.chat', ['stack.services'])
     })
       
     .controller('chatController', ['stackSocket', '$scope', '$rootScope', '$filter', 
-            '$routeParams', '$location', '$anchorScroll', '$timeout',
-        function (stackSocket, $scope, $rootScope, $filter, $routeParams, $location, $anchorScroll, $timeout){
+            '$routeParams', '$location', '$anchorScroll', '$timeout', '$window',
+        function (stackSocket, $scope, $rootScope, $filter, $routeParams, $location, $anchorScroll, $timeout, $window){
         var _this = this;
         $scope.last_id = false;
-        $scope.new_message = "";
         $scope.messages = [];
         $scope.showmore = false;
-        
-        $scope.add_message = function(){
-            console.log('add_message', $scope.new_message);
-            stackSocket.emit('chatroom message', {message: $scope.new_message});            
-            $scope.new_message = '';
-        };
-        stackSocket.on('chatroom message', function(data){
-            if(data.success){
-                $scope.messages.push(data.message);
-                if(data.message.userid === $scope.userid){
-                    $scope.scrollDown();
-                }
-            }
-        });
-        
-        $scope.fetch_messages = function(){
-            stackSocket.emit('chatroom get messages', {last_id: $scope.last_id});
-        }
-        
-        console.log('chat');
-        
+        $scope.connected = false;
         
         this.init = function(){
+            this.on_connect();
             this.join();
         };
+        
+        $rootScope.$on('stack:socket connected', function(){
+            _this.init();
+            $scope.connected = true;
+            $scope.scrollDown();
+        });
+        
+        $rootScope.$on('stack:socket disconnected', function(){
+            $scope.connected = false;
+            $scope.scrollUp();
+        });
         
         this.join = function(){
             
@@ -73,50 +64,98 @@ angular.module('stack.chat', ['stack.services'])
                 username: $scope.username,
                 userid: $scope.userid
             });
-        };        
-        stackSocket.on('chatroom join', function(data){
-            console.log('chatroom join', data);
-            if(data.success){
-                $scope.fetch_messages();
-            }
-        });
-        
-        stackSocket.on('chatroom get messages', function(data){
-            console.log('chatroom get messages', data);
-            if(data.success){
-                if(data.messages && data.messages.length >0){
-                    var messages = $filter('orderBy')(data.messages, '_id');
-                    var last_id = messages[0]._id;
-                    
-                    if(!$scope.last_id || last_id < $scope.last_id){
-                        $scope.messages = $scope.messages.concat(data.messages);
-                        if(!$scope.last_id){
-                            $scope.scrollDown();
-                        }else{
-                            $scope.scrollDown('chat_'+$scope.last_id);
-                        }
-                        $scope.last_id = last_id;
-                        $scope.showmore = (data.pagesize === data.messages.length);
-                    }
-                }else{
-                    $scope.showmore = false;
-                }
-            }
-        });
-        
-        stackSocket.on('chatroom users present', function(data){
-            console.log('chatroom users present', data);
-        });
-        
-        $scope.scrollDown = function(){
-            $timeout(function() {
-                $location.hash('bottom');
-                $anchorScroll();
-            });
         };
         
-        this.init();
+        this.on_connect = function(){
+            stackSocket.on('stack api connect', function(data){
+                console.log('stack api connect');
+                _this.init();
+            });
+            
+            stackSocket.on('chatroom message', function(data){
+                if(data.success){
+                    $scope.messages.push(data.message);
+                    if(data.message.userid === $scope.userid){
+                        $scope.scrollDown();
+                    }
+                }
+            });
+            
+            stackSocket.on('chatroom join', function(data){
+                console.log('chatroom join', data);
+                if(data.success){
+                    $scope.fetch_messages();
+                }
+            });
+
+            stackSocket.on('chatroom get messages', function(data){
+                console.log('chatroom get messages', data);
+                if(data.success){
+                    if(data.messages && data.messages.length >0){
+                        var messages = $filter('orderBy')(data.messages, '_id');
+                        var last_id = messages[0]._id;
+
+                        if(!$scope.last_id || last_id < $scope.last_id){
+                            $scope.messages = $scope.messages.concat(data.messages);
+                            if(!$scope.last_id){
+                                $scope.scrollDown();
+                            }else{
+                                $scope.scrollDown('chat_'+$scope.last_id);
+                            }
+                            $scope.last_id = last_id;
+                            $scope.showmore = (data.pagesize === data.messages.length);
+                        }
+                    }else{
+                        $scope.showmore = false;
+                    }
+                }
+            });
+
+            stackSocket.on('chatroom users present', function(data){
+                console.log('chatroom users present', data);
+            });
+        }
         
+        
+        $scope.fetch_messages = function(){
+            stackSocket.emit('chatroom get messages', {last_id: $scope.last_id});
+        };
+        
+        $scope.scrollDown = function(){
+            if($scope.connected){
+                $timeout(function() {
+                    $location.hash('chat-bottom-anchor');
+                    $anchorScroll();
+                });
+            }
+        };
+        
+        $scope.scrollUp = function(){
+            $window.scrollTo(0, 0);
+        };
+        
+        
+    }])
+
+    .controller('chatInputController', ['stackSocket', '$scope', '$rootScope',
+        function (stackSocket, $scope, $rootScope){
+        var _this = this;
+        $scope.new_message = "";
+        $scope.connected = false;
+        
+        $rootScope.$on('stack:socket connected', function(){
+            $scope.connected = true;
+        });
+        
+        $rootScope.$on('stack:socket disconnected', function(){
+            $scope.connected = false;
+        });        
+        
+        $scope.add_message = function(){
+            console.log('add_message', $scope.new_message);
+            stackSocket.emit('chatroom message', {message: $scope.new_message});            
+            $scope.new_message = '';
+        };        
     }])
 
 
@@ -138,6 +177,15 @@ angular.module('stack.chat', ['stack.services'])
         return {
             restrict: "E",
             templateUrl: stack_settings.root + "dist/view/chat-item.html"
+        };
+    })
+    
+    .directive("stackChatInput", function (stack_settings) {
+        return {
+            restrict: "E",
+            templateUrl: stack_settings.root + "dist/view/chat-input.html",
+            controller: 'chatInputController',
+            controllerAs: 'chatInCtrl'
         };
     })
 ;
--- dist/stack.services.js
+++ dist/stack.services.js
@@ -4,42 +4,64 @@ angular.module('stack.services', ['btford.socket-io'])
     var _this = this;
     var socket = false;
     this.api_key = "V1fUDfShl";
-    this.stack_server = 'localhost:1339';
+    this.stack_server = 'http://localhost:1339';
     
-    this.connect = function(api_key){
-        if(!api_key) api_key = this.api_key;
+    this.connect = function(e, api_key){
+        if(!api_key) api_key = _this.api_key;
+        
         if(api_key){
-            socket = io.connect(this.stack_server,{query: 'key='+api_key+'&protocol='+$location.protocol()+'&host='+$location.host()+'&port='+$location.port()});
+            socket = io.connect(_this.stack_server,{query: 'key='+api_key+'&protocol='+$location.protocol()+'&host='+$location.host()+'&port='+$location.port()});
+        
+            this.on('stack api connect', function(){
+                console.log('stack api connect');
+                $rootScope.$broadcast('stack:socket connected');
+            });
         }else{
-            socket = io.connect();
+            console.log('stack.services: no api key');
         }
-    }
+        
+    };
     
-    $rootScope.$on('stack:socket connect', _this.connect);
-    this.connect();
+    this.disconnect = function(){
+        socket.io.close();
+        $rootScope.$broadcast('stack:socket disconnected');
+    };
     
-    return {
-        connect: this.connect,
-        on: function (eventName, callback) {
-            if(!socket){
-                console.log('not connected');
-            }
+    this.on = function (eventName, callback) {
+        if(!socket){
+            console.log('not connected');
+        }else{
             socket.on(eventName, function () {
                 var args = arguments;
                 $rootScope.$apply(function () {
                     callback.apply(socket, args);
                 });
             });
-        },
+        }
+    };
+    
+    
+    
+//    $rootScope.$on('stack:socket connect', _this.connect);
+    //this.connect();
+    
+    return {
+        connect: this.connect,
+        disconnect: this.disconnect,
+        on: this.on,
         emit: function (eventName, data, callback) {
-            socket.emit(eventName, data, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args);
-                    }
+            if(!socket){
+                console.log('not connected');
+            }else{
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
                 });
-            });
+            }
         },
         reset: function(){
             socket.disconnect();
